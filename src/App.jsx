93c8a120d676101a09fc62aabf1dfa81aace8066
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2025-12-11 16:32:34 -0300

    Adding <Typography> component inside a <div>

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Aside } from './components/Aside'
 import { Container } from './components/Container'
 import { Main } from './components/Main'
 import { SearchInput } from './components/Searchinput'
+import { Typography } from './components/Typography'
 
 function App() {
   return (
@@ -10,6 +11,14 @@ function App() {
       <Aside />
       <Main>
         <SearchInput />
+        <div>
+          <Typography variant="h1">
+            Olá, Flávio!
+          </Typography>
+          <Typography variant="body">
+            Veja como estão suas finanças hoje.
+          </Typography>
+        </div>
       </Main>
     </Container>
   )
