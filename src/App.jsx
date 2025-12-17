db6a701506f9b8bcbcbafdb0d5e305600ed9a70b
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2025-12-17 11:55:07 -0300

    Putting all components into a div that will container CSS

--- src/App.jsx
+++ src/App.jsx
@@ -7,55 +7,58 @@ import { Main } from './components/Main'
 import { SavingsStatus } from './components/SavingsStatus'
 import { SearchInput } from './components/Searchinput'
 import { Typography } from './components/Typography'
+import styles from './app.module.css'
 
 function App() {
   return (
     <Container>
       <Aside />
       <Main>
-        <SearchInput />
-        <div>
-          <Typography variant="h1">
-            Olá, Flávio!
-          </Typography>
-          <Typography variant="body">
-            Veja como estão suas finanças hoje.
-          </Typography>
+        <div className={styles.container}>
+          <SearchInput />
+          <div>
+            <Typography variant="h1">
+              Olá, Flávio!
+            </Typography>
+            <Typography variant="body">
+              Veja como estão suas finanças hoje.
+            </Typography>
+          </div>
+          <section className={styles.grid}>
+            <Card>
+              <Card.Header>
+                Orçamento diário disponível:
+              </Card.Header>
+              <Card.Body>
+                <DailyBudget value={250} />
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>
+                Progresso da meta financeira
+              </Card.Header>
+              <Card.Body>
+                <SavingsStatus percent={32} />
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>
+                Orçamento diário disponível:
+              </Card.Header>
+              <Card.Body>
+                R$200
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>
+                Orçamento diário disponível:
+              </Card.Header>
+              <Card.Body>
+                R$200
+              </Card.Body>
+            </Card>
+          </section>
         </div>
-        <section>
-          <Card>
-            <Card.Header>
-              Orçamento diário disponível:
-            </Card.Header>
-            <Card.Body>
-              <DailyBudget value={250} />
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Header>
-              Progresso da meta financeira
-            </Card.Header>
-            <Card.Body>
-              <SavingsStatus percent={25} />
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Header>
-              Orçamento diário disponível:
-            </Card.Header>
-            <Card.Body>
-              R$200
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Header>
-              Orçamento diário disponível:
-            </Card.Header>
-            <Card.Body>
-              R$200
-            </Card.Body>
-          </Card>
-        </section>
       </Main>
     </Container>
   )
